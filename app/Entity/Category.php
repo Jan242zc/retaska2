822cb5ba0499b979d80bce8586e78815f6441ae7
jfort <jfortunik24@centrum.cz>
2020-09-02 22:03:34 +0200

    category entity

---
+++ app/Entity/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+final class Category
+{
+	/** @var int */
+	private $id;
+
+	/** @var string */
+	private $name;
+	
+	public function __construct(int $id, string $name){
+		$this->id = $id;
+		$this->name = $name;
+	}
+	
+	public function getId(): int
+	{
+		return $this->id;
+	}
+	
+	public function getName(): string
+	{
+		return $this->name;
+	}
+	
+	public function setName(string $name): void
+	{
+		$this->name = $name;
+	}
+	
+	public function toArray(): Array
+	{
+		return [
+			'id' => $this->id,
+			'name' => $this->name
+		];
+	}
+}
