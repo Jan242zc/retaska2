5190d3fca03046163e0cb991c3055853a9d292aa
jfort <jfortunik24@centrum.cz>
2020-09-02 21:16:33 +0200

    basic category default page

---
+++ app/Modules/Admin/Presenters/CategoryPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Admin\Presenters;
+
+use Nette;
+use App\Modules\Admin\Presenters\BaseAdminPresenter as BasePresenter;
+
+
+final class CategoryPresenter extends BasePresenter
+{
+	public function renderDefault(): void
+	{
+		
+	}
+}
