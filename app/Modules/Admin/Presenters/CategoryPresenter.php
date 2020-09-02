f98362f5cb5f7a63bfc58a2e3053b538d555e22f
jfort <jfortunik24@centrum.cz>
2020-09-02 21:46:58 +0200

    manage form seems to be done

--- app/Modules/Admin/Presenters/CategoryPresenter.php
+++ app/Modules/Admin/Presenters/CategoryPresenter.php
@@ -6,6 +6,7 @@ namespace App\Modules\Admin\Presenters;
 
 use Nette;
 use App\Modules\Admin\Presenters\BaseAdminPresenter as BasePresenter;
+use Nette\Application\UI\Form;
 
 
 final class CategoryPresenter extends BasePresenter
@@ -18,5 +19,23 @@ final class CategoryPresenter extends BasePresenter
 	public function actionManage(): void
 	{
 		
+	}
+	
+	protected function createComponentManageCategoryForm(): Form
+	{
+		$form = new Form;
+		$form->setHtmlAttribute('class', 'form');
+				
+		$form->addHidden('id');
+		
+		$form->addText('name', 'Název:')
+			->setRequired('Kategorie musí mít název.');
+			
+		$form->addSubmit('submit', 'Uložit')
+			->setHtmlAttribute('class', 'submit');
+		
+		$form->onSuccess[] = [$this, 'manageCategoryFormSucceeded'];
+		
+		return $form;		
 	}
 }
