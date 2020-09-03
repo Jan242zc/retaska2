6b3c33df2bee2870764f6475f21b29cb7942e382
jfort <jfortunik24@centrum.cz>
2020-09-03 20:55:26 +0200

    presenter created

---
+++ app/Modules/Admin/Presenters/EntityPresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Admin\Presenters;
+
+use Nette;
+use App\Modules\Admin\Presenters\BaseAdminPresenter as BasePresenter;
+use Nette\Application\UI\Form;
+use App\Services\Repository\RepositoryInterface\IRepository;
+
+final class EntityPresenter extends BasePresenter
+{
+}
