bd3ee4fbfcfa18ea497dddb1fc55422f0b623c93
jfort <jfortunik24@centrum.cz>
2020-09-04 11:49:46 +0200

    entity factory

---
+++ app/Entity/Factory/EntityFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Factory;
+
+use App\Entity\Entity;
+
+
+final class EntityFactory
+{
+	public static function createFromArray(array $data): Entity
+	{
+		return new Entity($data['id'], $data['name'], $data['idLimit']);
+	}
+	
+	public static function createFromObject($object): Entity
+	{
+		return new Entity($object->id, $object->name, $object->idLimit);
+	}
+}
