51d22fde4ce02f76417cfff33388e053d0b34c3d
jfort <jfortunik24@centrum.cz>
2020-09-09 16:29:43 +0200

    FE navbar

---
+++ app/Controls/Front/NavbarControl.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controls\Front;
+
+use Nette;
+use Nette\Application\UI\Control;
+use App\Services\GenericStupidService;
+
+final class NavbarControl extends Control
+{
+	public function __construct(){
+
+	}
+	
+	public function render(): void
+	{
+		$this->template->render(__DIR__ . '/templates/navbar.latte');
+	}
+}
--- app/Modules/Front/Presenters/BaseFrontPresenter.php
+++ app/Modules/Front/Presenters/BaseFrontPresenter.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 namespace App\Modules\Front\Presenters;
 
 use Nette;
+use App\Controls\Front\NavbarControl;
+use App\Controls\Front\Factory\FrontNavbarFactory;
 
 
 abstract class BaseFrontPresenter extends Nette\Application\UI\Presenter
 {
-	
+	public function createComponentNavbarControl(): NavbarControl
+	{
+		$navbarControl = new NavbarControl();
+		$this->onStartup = [$navbarControl, 'render'];
+		return $navbarControl;
+	}
 }
