2846201654950e776574dd7dafe3762b40bcbdf3
jfort <jfortunik24@centrum.cz>
2020-09-18 17:13:25 +0200

    adding forgotten methods to IProdRep

--- app/Services/Repository/RepositoryInterface/IProductRepository.php
+++ app/Services/Repository/RepositoryInterface/IProductRepository.php
@@ -7,7 +7,9 @@ namespace App\Services\Repository\RepositoryInterface;
 
 interface IProductRepository
 {
+	public function findByCategory($category): Array;
 	public function getArrayOfUsedNames($currentProductId = null): Array;
 	public function getProductsCount(): int;
+	public function getProductsCountByCategory($category): int;
 	public function findTopProducts($limit): Array;
 }
