6f78e2ae0bc4367b96e0d734c9c26230edfbc8a6
jfort <jfortunik24@centrum.cz>
2020-09-21 15:21:23 +0200

    very basic basket editing

--- app/Modules/Front/Presenters/BasketPresenter.php
+++ app/Modules/Front/Presenters/BasketPresenter.php
@@ -7,6 +7,7 @@ namespace App\Modules\Front\Presenters;
 use Nette;
 use App\Modules\Front\Presenters\BaseFrontPresenter AS BasePresenter;
 use App\Services\GeneralServiceInterface\IBasketService;
+use Nette\Application\UI\Form;
 
 
 final class BasketPresenter extends BasePresenter
@@ -21,5 +22,30 @@ final class BasketPresenter extends BasePresenter
 	public function renderDefault(): void
 	{
 		$this->template->basketItems = $this->basketService->getAllBasketItems();
+	}
+	
+	protected function createComponentEditBasketForm(): Form
+	{
+		$form = new Form();
+		$form->setHtmlAttribute('class', 'add-to-basket border-all');
+		
+		$form->addSubmit('submit', 'Uložit')
+			->setHtmlAttribute('class', 'submit fullWidth');
+		
+		$form->onSuccess[] = [$this, 'editBasketFormSucceeded'];
+		
+		return $form;
+	}
+	
+	public function editBasketFormSucceeded(Form $form, Array $data)
+	{
+		dump($data);
+		$idValues = $form->getHttpData($form::DATA_LINE, 'id[]');
+		$quantityValues = $form->getHttpData($form::DATA_LINE, 'quantity[]');
+		$toBeDeletedValues = $form->getHttpData($form::DATA_TEXT, 'toBeDeleted[]');
+		dump($idValues);
+		dump($quantityValues);
+		dump($toBeDeletedValues);
+		exit;
 	}
 }
