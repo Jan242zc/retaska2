115aaccf22226a3e5238778487abc31850cef9c0
jfort <jfortunik24@centrum.cz>
2020-10-21 15:42:27 +0200

    purchase entity and factory

---
+++ app/Entity/Factory/PurchaseStatusFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Factory;
+
+use App\Entity\PurchaseStatus;
+
+
+final class PurchaseStatusFactory
+{
+	public static function createFromArray(array $data): PurchaseStatus
+	{
+		$meansCancelled = $data['means_cancelled'] ? false : true;
+		if(!$data['id']){
+			return new PurchaseStatus(null, $data['name'], $meansCancelled);
+		} else {
+			return new PurchaseStatus($data['id'], $data['name'], $meansCancelled);
+		}
+	}
+	
+	public static function createFromObject($object): PurchaseStatus
+	{
+		$meansCancelled = $object->means_cancelled ? false : true;
+		if(!$object->id){
+			return new PurchaseStatus(null, $object->name, $meansCancelled);
+		} else {
+			return new PurchaseStatus($object->id, $object->name, $meansCancelled);
+		}
+	}
+}
---
+++ app/Entity/PurchaseStatus.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+
+final class PurchaseStatus
+{
+	/** @var int*/
+	private $id;
+
+	/** @var string */
+	private $name;
+
+	/** @var bool */
+	private $meansCancelled;
+
+	public function __construct($id = null, string $name, bool $meansCancelled){
+		$this->id = $id;
+		$this->name = $name;
+		$this->meansCancelled = $meansCancelled;
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function setId(int $id): void
+	{
+		$this->id = $id;
+	}
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function setName(string $name): void
+	{
+		$this->name = $name;
+	}
+
+	public function getMeansCancelled(): bool
+	{
+		return $this->meansCancelled;
+	}
+
+	public function setMeansCancelled(bool $meansCancelled): void
+	{
+		$this->meansCancelled = $meansCancelled;
+	}
+}
