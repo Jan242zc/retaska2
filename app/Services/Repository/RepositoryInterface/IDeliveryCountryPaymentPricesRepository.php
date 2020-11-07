0a4d49b6c4a24b17704d10dbe72823225206029e
jfort <jfortunik24@centrum.cz>
2020-11-07 18:51:15 +0100

    IDeliveryCountryPaymentPrices

--- app/Services/Repository/RepositoryInterface/IDeliveryCountryPaymentPricesRepository.php
+++ app/Services/Repository/RepositoryInterface/IDeliveryCountryPaymentPricesRepository.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Services\Repository\RepositoryInterface;
 
+use App\Entity\DeliveryCountryPaymentPrices;
+
 
 interface IDeliveryCountryPaymentPricesRepository
 {
-	
+	public function findByDefiningStuff(int $deliveryId, int $paymentId, bool $countryIgnorable, int $countryId = null): DeliveryCountryPaymentPrices;
 }
