6c57b9ddf0c6c907f57c0fc6f778b154fe914796
jfort <jfortunik24@centrum.cz>
2020-11-08 19:08:58 +0100

    entity & role

---
+++ app/Entity/Factory/UserDataFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Factory;
+
+use App\Entity\UserData;
+use App\Entity\Role;
+use App\Services\Repository\RepositoryInterface\IRoleRepository;
+
+
+final class UserDataFactory
+{
+	/** @var IRoleRepository */
+	private $roleRepository;
+	
+	public function __construct(IRoleRepository $roleRepository){
+		$this->roleRepository = $roleRepository;
+	}
+	
+	public function createFromArray(Array $data): UserData
+	{
+		if(!$data['id']){
+			$data['id'] = null;
+		}
+		
+		try{
+			$role = $this->findRoleForUserData($data['role']);			
+		} catch(\Exception $ex){
+			throw $ex;
+		}
+		
+		return new UserData($data['id'], $data['name'], $role);
+	}
+	
+	public function createFromObject($object): UserData
+	{
+		if(!$object->id){
+			$object->id = null;
+		}
+		
+		try{
+			$role = $this->findRoleForUserData($object->role);
+		} catch(\Exception $ex){
+			throw $ex;
+		}
+		
+		return new UserData($object->id, $object->name, $role);
+	}
+	
+	private function findRoleForUserData($id): Role
+	{
+		try{
+			return $this->roleRepository->findById(intval($id));			
+		} catch(\Exception $ex){
+			throw $ex;
+		}
+	}
+}
---
+++ app/Entity/UserData.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Entity\Role;
+
+
+final class UserData
+{
+	/** @var int */
+	private $id;
+	
+	/** @var string */
+	private $name;
+	
+	/** @var Role */
+	private $role;
+	
+	public function __construct($id, string $name, Role $role){
+		$this->id = $id;
+		$this->name = $name;
+		$this->role = $role;
+	}
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	public function setId(int $id): void
+	{
+		$this->id = $id;
+	}
+	
+	public function getName(): int
+	{
+		return $this->id;
+	}
+	
+	public function setName(string $name): void
+	{
+		$this->name = $name;
+	}
+	
+	public function getRole(): Role
+	{
+		return $this->role;
+	}
+	
+	public function setRole(Role $role): void
+	{
+		$this->role = $role;
+	}
+	
+	public function toArray(): Array
+	{
+		return [
+			'id' => $this->id,
+			'name' => $this->name,
+			'role' => $this->role->getId()
+		];
+	}
+}
